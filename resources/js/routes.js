54605e6bea07a61dbcd69a1de629f33c3ee3caee
root <root@localhost.localdomain>
2020-09-08 16:07:53 +0800

    vue 設為 history 模式，laravel 做對應設定

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,16 +1,24 @@
 import VueRouter from "vue-router";
 import ExampleComponent from "./components/ExampleComponent";
+import Example2 from "./components/Example2";
 
 const routes = [
     {
         path: "/",
         component: ExampleComponent,
         name: "home"
+    },
+    {
+        path: "/second",
+        component: Example2,
+        name: "second"
     }
 ];
 
 const router = new VueRouter({
-    routes // (縮寫) 相當於 routes: routes
+    mode: "history",
+    routes, // (縮寫) 相當於 routes: routes
+    base: "/laravelbnb/public/"
 });
 
 export default router;
