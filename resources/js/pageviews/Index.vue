d4cf3b94e235d2a61802ad643083d486989196ed
stevecyj <ycsteven.tsao@gmail.com>
2020-09-15 11:24:09 +0800

    vue router parameters(finish)

--- resources/js/pageviews/Index.vue
+++ resources/js/pageviews/Index.vue
@@ -4,8 +4,8 @@
             <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }"
                 >LaravelBnb</router-link
             >
-            <router-link class="btn nav-button" :to="{ name: 'second' }"
-                >Second</router-link
+            <router-link class="btn nav-button" :to="{ name: 'bookable' }"
+                >bookable</router-link
             >
         </nav>
         <div class="container mt-4 mb-4 pr-4 pl-4">
---
+++ resources/js/test/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        I am test.
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            bookables: null
+        };
+    },
+    created() {
+        console.log(this.$route.params.id);
+        axios.get("/laravelbnb/public/api/bookables").then(response => {
+            this.bookables = response.data;
+            this.loading = false;
+            console.log(response);
+        });
+    }
+};
+</script>
