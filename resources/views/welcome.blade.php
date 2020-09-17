df5cf3313975fa2cd02ecc10aa24aea70c78cdfb
root <root@localhost.localdomain>
2020-09-17 20:34:06 +0800

    debug sourceMap,favicon error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link rel="shortcut icon" href="#"/>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset('js/app.js') }}" defer></script>
 
