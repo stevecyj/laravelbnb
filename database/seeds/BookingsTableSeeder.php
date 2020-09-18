c9f04e6159c8d08937bb5c74448bca73192f098b
stevecyj <ycsteven.tsao@gmail.com>
2020-09-18 17:13:26 +0800

    51. Seeding bookings (generating random, non overlapping time series)(to be continued)

---
+++ database/seeds/BookingsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BookingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bookable::all()->each(function (Bookable $bookable) {
+            $booking = factory(Booking::class)->make();
+            $bookings = collect([$booking]);
+        });
+    }
+}
