18086508b914eaa1ae16a3c44b3555eeafdb2153
stevecyj <ycsteven.tsao@gmail.com>
2020-09-18 12:31:10 +0800

    missing param for named route

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card w-100">
         <div class="card-body">
-            <router-link :to="{ name: 'bookable', params: { id } }">
+            <router-link v-if="id" :to="{ name: 'bookable', params: { id } }">
                 <h5 class="card-title">{{ title }}</h5>
             </router-link>
             <p class="card-text">{{ description }}</p>
@@ -16,10 +16,11 @@ export default {
             isloading: false,
         };
     },
-
     props: { title: String, description: String, id: Number },
-    mounted() {
-        this.loading = true;
+    creted() {
+        setTimeout(() => {
+            this.isloading = true;
+        }, 500);
     },
 };
 </script>
